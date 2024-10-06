092139bf34e1bb5dc90461c7fd8e7bac6f8d28f2
miyemicodes <lizzymiemi@gmail.com>
2024-10-06 21:35:47 +0100

    updated

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,60 +2,66 @@ import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import NewColumnPopUp from "./PopUp/NewColumnPopup";
 import TaskCard from "./Taskcard";
-import { useStoreContext } from '../context/Store-context';
+import { useStoreContext } from "../context/Store-context";
 
 function Board() {
-  const [showColumnPopup, setColumnPopup] = useState(false);
-  // const [editColuData,] = useState()
-  const [store] = useStoreContext();
+	const [showColumnPopup, setColumnPopup] = useState(false);
+	// const [editColuData,] = useState()
+	const [store] = useStoreContext();
 
-  const onClick = () => setColumnPopup(true);
-  const handleToggle = () => setColumnPopup(false);
-  
-  function colData(columnData,task){
-     console.log(columnData.id,task);
-  }
+	const onClick = () => setColumnPopup(true);
+	const handleToggle = () => setColumnPopup(false);
 
-  return (
-    <>
-      <section className='bg-[#22212c] h-auto min-h-[250px] w-3/4 flex flex-row flex-shrink-0 gap-8 overflow-x-auto p-8'>
-        {store.map((columnData, index) => (
-          <div
-            key={index}
-            className=' p-4 w-[350px] rounded-lg border-dashed border-2 flex-shrink-0 border-[#808fa4]'
-          >
-            <div className="flex flex-row align-center justify-between">
-              <p className="text-white text-3xl">{columnData.label}</p>
-              <span className="text-2xl text-white">({columnData.tasks.length})</span>  
-            </div>
-          
-            {columnData.tasks.map((task) => (              
-              <div key={task.id}  onClick = {()=> colData(columnData.id,task)}>
-                 <TaskCard key={task.id} taskData={task} />
-              </div>
-             
-            ))}
-          </div>
-        ))}
+	function colData(columnData, task) {
+		console.log(columnData.id, task);
+	}
 
-        <div className=' p-4 w-[350px] rounded-lg flex items-center justify-center bg-gradient-to-b from-[#30323d] to-transparent'>
-          <button
-            type='submit'
-            onClick={onClick}
-            className='flex self-center justify-center gap-2 font-semibold text-2xl text-[#808fa4]'
-          >
-            <FaPlus className='text-2xl' />
-            <p> New Column</p>
-          </button>
-        </div>
+	return (
+		<>
+			<section className="bg-[#22212c] h-auto min-h-[250px] w-3/4 flex flex-row flex-shrink-0 gap-8 overflow-x-auto p-8">
+				{store.map((columnData, index) => (
+					<div
+						key={index}
+						className=" p-4 w-[350px] rounded-lg border-dashed border-2 flex-shrink-0 border-[#808fa4]"
+					>
+						<div className="flex flex-row align-center justify-between">
+							<p className="text-white text-3xl">{columnData.label}</p>
+							<span className="text-2xl text-white">
+								({columnData.tasks.length})
+							</span>
+						</div>
 
-      </section>
+						{columnData.tasks.map((task) => (
+							<div
+								key={task.id}
+								onClick={() => colData(columnData.id, task)}
+							>
+								<TaskCard
+									key={task.id}
+									taskData={task}
+								/>
+							</div>
+						))}
+					</div>
+				))}
 
-      {showColumnPopup ? (
-        <NewColumnPopUp handleCloseModal={() => handleToggle()} />
-      ) : null}
-    </>
-  );
+				<div className=" p-4 w-[350px] rounded-lg flex items-center justify-center bg-gradient-to-b from-[#30323d] to-transparent">
+					<button
+						type="submit"
+						onClick={onClick}
+						className="flex self-center justify-center gap-2 font-semibold text-2xl text-[#808fa4]"
+					>
+						<FaPlus className="text-2xl" />
+						<p> New Column</p>
+					</button>
+				</div>
+			</section>
+
+			{showColumnPopup ? (
+				<NewColumnPopUp handleCloseModal={() => handleToggle()} />
+			) : null}
+		</>
+	);
 }
 
 export default Board;
--- src/context/Store-context.jsx
+++ src/context/Store-context.jsx
@@ -1,25 +1,25 @@
-import React, { useState, useContext } from 'react';
-import { createContext } from 'react';
-import { DefaultData } from './Datastructure';
+import React, { useState, useContext } from "react";
+import { createContext } from "react";
+import { DefaultData } from "./Datastructure";
 
 export const StoreContext = createContext(null);
 
 export default function StoreContextProvider({ children }) {
-  const [mainState, setMainState] = useState(DefaultData);
+	const [mainState, setMainState] = useState(DefaultData);
 
-  return (
-    <StoreContext.Provider value={[mainState, setMainState]}>
-      {children}
-    </StoreContext.Provider>
-  );
+	return (
+		<StoreContext.Provider value={[mainState, setMainState]}>
+			{children}
+		</StoreContext.Provider>
+	);
 }
 
 export function useStoreContext() {
-  const context = useContext(StoreContext);
-  if (!context) {
-    throw new Error(
-      'useStoreContext must be used withthin StoreContextProvider'
-    );
-  }
-  return context;
+	const context = useContext(StoreContext);
+	if (!context) {
+		throw new Error(
+			"useStoreContext must be used withthin StoreContextProvider"
+		);
+	}
+	return context;
 }
